c885b8464d1a3416630f4c018831329a2880eb24
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2024-09-12 21:37:16 +0200

    3Dpong threejs @max Fixes #2

---
+++ pong/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Clean the output directory before emit
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true, // Automatically open the browser
+  },
+};
