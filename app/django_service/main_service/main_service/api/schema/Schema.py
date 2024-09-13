6aded69a436918a75299e3567f626911cd588396
nscheefe <nils.scheefe@gmail.com>
2024-09-13 23:36:20 +0200

    [adminService] Add admin service add grpc and proto add grpchandler Update main service include admin service proto and schema

--- app/django_service/main_service/main_service/api/schema/Schema.py
+++ app/django_service/main_service/main_service/api/schema/Schema.py
@@ -5,10 +5,12 @@ from .statSchema import Query as StatQuery, Mutation as StatMutation
 from .gameSchema import Query as gameQuery, Mutation as gameMutation
 from .chatSchema import Query as chatQuery, Mutation as chatMutation
 from .authSchema import Query as authQuery, Mutation as authMutation
-class Query(UserQuery, StatQuery, gameQuery,chatQuery,authQuery, graphene.ObjectType):
+from .adminSchema import Query as adminQuery, Mutation as adminMutation
+
+class Query(UserQuery, StatQuery, gameQuery,chatQuery,authQuery,adminQuery, graphene.ObjectType):
     pass
 
-class Mutation(UserMutation, StatMutation, gameMutation, chatMutation,authMutation, graphene.ObjectType):
+class Mutation(UserMutation, StatMutation, gameMutation, chatMutation,authMutation,adminMutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
