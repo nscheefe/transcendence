aa07b0744d98fc50179275ff15907c4c2a376868
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2024-11-11 12:02:47 +0100

    refactor: Refactored Go ServerLogic to several files

---
+++ server-side-pong/gameLogic.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"math/rand"
+	"math"
+	"time"
+)
+
+func resetBall() {
+ randomX := (rand.Float64() - 0.5) * 9
+ randomZ := 0.0
+
+ gameState.Ball = Position{X: randomX, Y: 0.5, Z: randomZ}
+
+ baseAngle := math.Pi / 2
+ if rand.Float64() < 0.5 {
+     baseAngle = -math.Pi / 2
+ }
+ randomAngle := baseAngle + (rand.Float64()*2-1)*maxAngleVariation
+ gameState.BallSpeed = Speed{
+     X: initialBallSpeed * math.Cos(randomAngle),
+     Z: initialBallSpeed * math.Sin(randomAngle),
+ }
+ gameState.Direction = 1
+ if gameState.BallSpeed.Z < 0 {
+     gameState.Direction = -1
+ }
+}
+
+func checkWinner() {
+ if gameState.Points.Player1 >= winningPoints || gameState.Points.Player2 >= winningPoints {
+     gameStarted = false
+     gameLoopInterval.Stop()
+     broadcast(map[string]interface{}{
+         "type":   "gameOver",
+         "winner": 1,
+     })
+     if gameState.Points.Player2 >= winningPoints {
+         broadcast(map[string]interface{}{
+             "type":   "gameOver",
+             "winner": 2,
+         })
+     }
+     gameState.Points.Player1 = 0
+     gameState.Points.Player2 = 0
+ }
+}
+
+func updatePaddlePositions() {
+ mu.Lock()
+ defer mu.Unlock()
+ for _, client := range clients {
+     if client.role == "paddle1" {
+         if gameState.KeyState["ArrowLeft"] && gameState.Paddle1.X > -4.5 {
+             gameState.Paddle1.X -= 0.1
+         }
+         if gameState.KeyState["ArrowRight"] && gameState.Paddle1.X < 4.5 {
+             gameState.Paddle1.X += 0.1
+         }
+     } else if client.role == "paddle2" {
+         if gameState.KeyState["a"] && gameState.Paddle2.X > -4.5 {
+             gameState.Paddle2.X -= 0.1
+         }
+         if gameState.KeyState["d"] && gameState.Paddle2.X < 4.5 {
+             gameState.Paddle2.X += 0.1
+         }
+     }
+ }
+}
+
+func gameLoop() {
+ if !gameStarted {
+     return
+ }
+
+ updatePaddlePositions()
+
+ gameState.Ball.X += gameState.BallSpeed.X
+ gameState.Ball.Z += gameState.BallSpeed.Z
+
+ if gameState.Ball.X <= -4.5 || gameState.Ball.X >= 4.5 {
+     gameState.BallSpeed.X = -gameState.BallSpeed.X
+ }
+
+ if gameState.Ball.Z <= -8.5 && gameState.Ball.Z >= -9 && gameState.Ball.X >= gameState.Paddle1.X-1 && gameState.Ball.X <= gameState.Paddle1.X+1 {
+     gameState.BallSpeed.Z = -gameState.BallSpeed.Z
+     impactPoint := gameState.Ball.X - gameState.Paddle1.X
+     gameState.BallSpeed.X += impactPoint * 0.05
+     gameState.Direction = 1
+     if gameState.BallSpeed.Z < 0 {
+         gameState.Direction = -1
+     }
+ }
+ if gameState.Ball.Z >= 8.5 && gameState.Ball.Z <= 9 && gameState.Ball.X >= gameState.Paddle2.X-1 && gameState.Ball.X <= gameState.Paddle2.X+1 {
+     gameState.BallSpeed.Z = -gameState.BallSpeed.Z
+     impactPoint := gameState.Ball.X - gameState.Paddle2.X
+     gameState.BallSpeed.X += impactPoint * 0.05
+     gameState.Direction = 1
+     if gameState.BallSpeed.Z < 0 {
+         gameState.Direction = -1
+     }
+ }
+
+ if gameState.Ball.Z <= -10 {
+     gameState.Points.Player2++
+     resetBall()
+     checkWinner()
+ } else if gameState.Ball.Z >= 10 {
+     gameState.Points.Player1++
+     resetBall()
+     checkWinner()
+ }
+
+ broadcast(map[string]interface{}{
+     "type":    "updateState",
+     "ball":    gameState.Ball,
+     "paddle1": gameState.Paddle1,
+     "paddle2": gameState.Paddle2,
+     "points":  gameState.Points,
+     "direction": gameState.Direction,
+ })
+}
+
+func startGame() {
+ gameStarted = true
+ gameLoopInterval = time.NewTicker(time.Second / 60)
+ go func() {
+     for range gameLoopInterval.C {
+         gameLoop()
+     }
+ }()
+ broadcast(map[string]interface{}{
+     "type": "gameStart",
+ })
+}
--- server-side-pong/main.go
+++ server-side-pong/main.go
@@ -3,11 +3,9 @@ package main
 import (
     "fmt"
     "math"
-    "math/rand"
     "net/http"
     "sync"
     "time"
-
     "github.com/gorilla/websocket"
 )
 
@@ -35,9 +33,14 @@ var (
             return true
         },
     }
-    clients = make(map[*websocket.Conn]bool)
+    clients = make(map[*websocket.Conn]*Client)
     mu      sync.Mutex
 )
+
+type Client struct {
+    conn *websocket.Conn
+    role string // "paddle1" or "paddle2"
+}
 
 type Position struct {
     X float64 `json:"x"`
@@ -63,173 +66,6 @@ type GameState struct {
     Points    Points           `json:"points"`
     KeyState  map[string]bool  `json:"keyState"`
     Direction int              `json:"direction"`
-}
-
-func resetBall() {
-    randomX := (rand.Float64() - 0.5) * 9
-    randomZ := 0.0
-
-    gameState.Ball = Position{X: randomX, Y: 0.5, Z: randomZ}
-
-    baseAngle := math.Pi / 2
-    if rand.Float64() < 0.5 {
-        baseAngle = -math.Pi / 2
-    }
-    randomAngle := baseAngle + (rand.Float64()*2-1)*maxAngleVariation
-    gameState.BallSpeed = Speed{
-        X: initialBallSpeed * math.Cos(randomAngle),
-        Z: initialBallSpeed * math.Sin(randomAngle),
-    }
-    gameState.Direction = 1
-    if gameState.BallSpeed.Z < 0 {
-        gameState.Direction = -1
-    }
-}
-
-func checkWinner() {
-    if gameState.Points.Player1 >= winningPoints || gameState.Points.Player2 >= winningPoints {
-        gameStarted = false
-        gameLoopInterval.Stop()
-        broadcast(map[string]interface{}{
-            "type":   "gameOver",
-            "winner": 1,
-        })
-        if gameState.Points.Player2 >= winningPoints {
-            broadcast(map[string]interface{}{
-                "type":   "gameOver",
-                "winner": 2,
-            })
-        }
-        gameState.Points.Player1 = 0
-        gameState.Points.Player2 = 0
-    }
-}
-
-func updatePaddlePositions() {
-    if gameState.KeyState["ArrowLeft"] && gameState.Paddle1.X > -4.5 {
-        gameState.Paddle1.X -= 0.1
-    }
-    if gameState.KeyState["ArrowRight"] && gameState.Paddle1.X < 4.5 {
-        gameState.Paddle1.X += 0.1
-    }
-    if gameState.KeyState["a"] && gameState.Paddle2.X > -4.5 {
-        gameState.Paddle2.X -= 0.1
-    }
-    if gameState.KeyState["d"] && gameState.Paddle2.X < 4.5 {
-        gameState.Paddle2.X += 0.1
-    }
-}
-
-func gameLoop() {
-    if !gameStarted {
-        return
-    }
-
-    updatePaddlePositions()
-
-    gameState.Ball.X += gameState.BallSpeed.X
-    gameState.Ball.Z += gameState.BallSpeed.Z
-
-    if gameState.Ball.X <= -4.5 || gameState.Ball.X >= 4.5 {
-        gameState.BallSpeed.X = -gameState.BallSpeed.X
-    }
-
-    if gameState.Ball.Z <= -8.5 && gameState.Ball.Z >= -9 && gameState.Ball.X >= gameState.Paddle1.X-1 && gameState.Ball.X <= gameState.Paddle1.X+1 {
-        gameState.BallSpeed.Z = -gameState.BallSpeed.Z
-        impactPoint := gameState.Ball.X - gameState.Paddle1.X
-        gameState.BallSpeed.X += impactPoint * 0.05
-        gameState.Direction = 1
-        if gameState.BallSpeed.Z < 0 {
-            gameState.Direction = -1
-        }
-    }
-    if gameState.Ball.Z >= 8.5 && gameState.Ball.Z <= 9 && gameState.Ball.X >= gameState.Paddle2.X-1 && gameState.Ball.X <= gameState.Paddle2.X+1 {
-        gameState.BallSpeed.Z = -gameState.BallSpeed.Z
-        impactPoint := gameState.Ball.X - gameState.Paddle2.X
-        gameState.BallSpeed.X += impactPoint * 0.05
-        gameState.Direction = 1
-        if gameState.BallSpeed.Z < 0 {
-            gameState.Direction = -1
-        }
-    }
-
-    if gameState.Ball.Z <= -10 {
-        gameState.Points.Player2++
-        resetBall()
-        checkWinner()
-    } else if gameState.Ball.Z >= 10 {
-        gameState.Points.Player1++
-        resetBall()
-        checkWinner()
-    }
-
-    broadcast(map[string]interface{}{
-        "type":    "updateState",
-        "ball":    gameState.Ball,
-        "paddle1": gameState.Paddle1,
-        "paddle2": gameState.Paddle2,
-        "points":  gameState.Points,
-        "direction": gameState.Direction,
-    })
-}
-
-func startGame() {
-    gameStarted = true
-    gameLoopInterval = time.NewTicker(time.Second / 60)
-    go func() {
-        for range gameLoopInterval.C {
-            gameLoop()
-        }
-    }()
-}
-
-func broadcast(message interface{}) {
-    mu.Lock()
-    defer mu.Unlock()
-    for client := range clients {
-        err := client.WriteJSON(message)
-        if err != nil {
-            client.Close()
-            delete(clients, client)
-        }
-    }
-}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-    ws, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer ws.Close()
-
-    mu.Lock()
-    clients[ws] = true
-    mu.Unlock()
-
-    fmt.Println("Client connected")
-
-    for {
-        var msg map[string]interface{}
-        err := ws.ReadJSON(&msg)
-        if err != nil {
-            fmt.Println("Client disconnected")
-            mu.Lock()
-            delete(clients, ws)
-            mu.Unlock()
-            break
-        }
-
-        if msg["type"] == "keyState" {
-            key := msg["key"].(string)
-            state := msg["state"].(bool)
-            gameState.KeyState[key] = state
-        }
-    }
-
-    if !gameStarted {
-        startGame()
-    }
 }
 
 func main() {
