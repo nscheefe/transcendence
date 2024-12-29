d777acbd2679e0ae5f292c42e13775cd68234eab
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2024-12-29 16:28:57 +0100

    Add create-account template and update URL routing for registration and home

--- app/django_service/main_service/main_service/frontend/views.py
+++ app/django_service/main_service/main_service/frontend/views.py
@@ -6,3 +6,9 @@ def index(request):
     View that handles the root URL `/` and renders the index.html template.
     """
     return render(request, 'frontend/sign-in.html')
+
+def home(request):
+	return render(request, 'frontend/index.html')
+
+def register(request):
+	return render(request, 'frontend/create-account.html')
