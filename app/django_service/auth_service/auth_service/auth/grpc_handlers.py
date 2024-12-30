27ec6dc8ee971ed04ca213c78c22d7396afbd6bb
pgrossma <pgrossma@student.42heilbronn.de>
2024-12-30 21:02:30 +0000

    [auth] working revalidation of invalid tokens

--- app/django_service/auth_service/auth_service/auth/grpc_handlers.py
+++ app/django_service/auth_service/auth_service/auth/grpc_handlers.py
@@ -1,14 +1,10 @@
 import grpc
-from concurrent import futures
 from auth_service.protos import auth_pb2_grpc, auth_pb2
-from auth_service.auth.models import Auth
 from django.conf import settings
 import jwt
-import requests
-from auth_service.utils import generate_random_string
 from auth_service.auth.auth_db_operations import *
 from auth_service.auth.intra_api import *
-from datetime import datetime, timezone, timedelta
+from datetime import datetime
 
 class AuthServiceHandler(auth_pb2_grpc.AuthServiceServicer):
     def __init__(self):
@@ -16,16 +12,21 @@ class AuthServiceHandler(auth_pb2_grpc.AuthServiceServicer):
 
     def ExchangeCodeForToken(self, request, context):
         token_data = exchange_code_for_token(request.code, request.state)
-        access_token = token_data.get('access_token')
-        if not access_token:
+        if not token_data.get('access_token'):
             context.set_code(grpc.StatusCode.UNAUTHENTICATED)
             context.set_details('Invalid authorization code')
             return auth_pb2.ExchangeCodeResponse()
 
-        user_info = get_user_info(access_token)
-        auth_id = insert_auth_record(access_token, user_info.get('id'))
+        user_info = get_user_info(token_data.get('access_token'))
+        if not user_info:
+            context.set_code(grpc.StatusCode.UNAUTHENTICATED)
+            context.set_details('Invalid access token')
+            return auth_pb2.ExchangeCodeResponse()
 
-        jwt_token = jwt.encode({'auth_id': auth_id, 'user_id': user_info.get('id'), 'exp': datetime.now(timezone.utc) + timedelta(seconds=7200)}, settings.JWT_SECRET, algorithm='HS256')
+        expires_at = datetime.fromtimestamp(token_data.get('created_at') + token_data.get('expires_in'))
+        auth_id = insert_auth_record(token_data.get('access_token'), token_data.get('refresh_token'), expires_at, user_info.get('id'))
+
+        jwt_token = jwt.encode({'auth_id': auth_id, 'user_id': user_info.get('id')}, settings.JWT_SECRET, algorithm='HS256')
 
         return auth_pb2.ExchangeCodeResponse(jwt_token=jwt_token, name=user_info.get('login'), mail=user_info.get('email'), user_id=user_info.get('id'))
     
@@ -43,7 +44,7 @@ class AuthServiceHandler(auth_pb2_grpc.AuthServiceServicer):
             context.set_details('Invalid token')
             return auth_pb2.GetUserIDFromJwtTokenResponse()
 
-        valid_auth = auth_id_exists(auth_id)
+        valid_auth = validate_auth_id(auth_id)
         if not valid_auth:
             context.set_code(grpc.StatusCode.UNAUTHENTICATED)
             context.set_details('No auth record found')
--- app/django_service/auth_service/auth_service/auth/models.py
+++ app/django_service/auth_service/auth_service/auth/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Auth(models.Model):
     id = models.AutoField(primary_key=True)
-    token = models.CharField(max_length=255)
     user_id = models.IntegerField()
-
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
+    expires_at = models.DateTimeField()
