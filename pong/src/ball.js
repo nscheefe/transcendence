7c169ef7a07fa6f369e41b48f36fd7775ee9231b
pgrossma <pgrossma@student.42heilbronn.de>
2025-01-04 13:33:23 +0000

    [old pong] rm pong folder as it is now in one file contained

--- pong/src/ball.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-
-export function createBall() {
-  const ballGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-  const ballMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-  const ball = new THREE.Mesh(ballGeometry, ballMaterial);
-  ball.position.set(0, 0.5, 0);
-  return ball;
-}
