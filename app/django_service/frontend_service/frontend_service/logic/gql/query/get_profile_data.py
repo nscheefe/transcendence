c05cffe1c692e59c955875f8b6b1349054404884
nscheefe <nils.scheefe@gmail.com>
2025-01-12 13:31:39 +0100

    [Frontend-Service] Add community feature with profile and friends management

---
+++ app/django_service/frontend_service/frontend_service/logic/gql/query/get_profile_data.py
@@ -0,0 +1,15 @@
+from frontend_service.logic.gql.basics import load_query, execute_query
+
+
+def getProfileData(request, id):
+    # Load the query from the file
+    query = load_query('query/getProfileData.gql')
+
+    # Define the variables for the GraphQL query
+    variables = {
+        "userId": id  # Pass the user ID as the variable
+    }
+
+    # Execute the query with variables
+    response = execute_query(query, request, variables=variables)
+    return response["profile"]
