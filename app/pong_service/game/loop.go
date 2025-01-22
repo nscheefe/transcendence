2b9d3e756e4d9e7106523fd361662a2d248ad6a2
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2025-01-22 01:46:01 +0000

    [Game_service] Update: Change winnerId type to int and ensure correct handling in game completion

--- app/pong_service/game/loop.go
+++ app/pong_service/game/loop.go
@@ -179,7 +179,7 @@ func resetBall(game *Game) {
 }
 
 func checkWinner(game *Game) {
-	var winnerId int32
+	winnerId := 0
 	if game.state.Points.Player1 >= winningPoints || game.state.Points.Player2 >= winningPoints {
 		game.State = GameStateFinished
 		if game.state.Points.Player1 >= winningPoints {
@@ -187,15 +187,15 @@ func checkWinner(game *Game) {
 				"type":   "gameOver",
 				"winner": 1,
 			})
-			winnerId = int32(game.info.PlayerAId)
+			winnerId = int(game.info.PlayerAId)
 		} else if game.state.Points.Player2 >= winningPoints {
 			broadcast(game, map[string]interface{}{
 				"type":   "gameOver",
 				"winner": 2,
 			})
-			winnerId = int32(game.info.PlayerBId)
+			winnerId = int(game.info.PlayerBId)
 		}
-		err := grpc.GameCon.HandleGameFinished(int32(game.id), int32(game.state.Points.Player1), int32(game.state.Points.Player2), winnerId)
+		err := grpc.GameCon.HandleGameFinished(int32(game.id), int32(game.state.Points.Player1), int32(game.state.Points.Player2), int32(winnerId))
 		if err != nil {
 			logGame(game, "GRPC error handling game finished: "+err.Error())
 		}
