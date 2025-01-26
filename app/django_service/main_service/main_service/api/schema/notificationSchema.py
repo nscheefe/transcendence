f294199adc9aaac3ce04ef26b6fab53062878222
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2025-01-26 21:24:21 +0000

    [main_service] Add: enhance online status check

--- app/django_service/main_service/main_service/api/schema/notificationSchema.py
+++ app/django_service/main_service/main_service/api/schema/notificationSchema.py
@@ -6,7 +6,8 @@ import logging
 import grpc
 from google.protobuf.timestamp_pb2 import Timestamp
 
-from ariadne import ObjectType
+from ariadne import ObjectType, SubscriptionType
+from channels.layers import get_channel_layer
 from main_service.api.schema.objectTypes import query, mutation, subscription
 
 from main_service.api.schema.userSchema import resolver as user_resolver
@@ -20,6 +21,8 @@ GRPC_PORT = "50051"
 GRPC_TARGET = f"{GRPC_HOST}:{GRPC_PORT}"
 
 logger = logging.getLogger(__name__)
+
+subscription = SubscriptionType()
 
 @subscription.source("notificationsForUser")
 async def notifications_for_user_source(_, info):
@@ -77,12 +80,13 @@ async def online_status_source(_, info, user_id):
             response = await user_stub.GetUser(grpc_request)
             last_login = response.last_login.ToDatetime() if response.HasField("last_login") else None
             is_online = last_login and (datetime.utcnow() - last_login) < timedelta(minutes=1)
+            if is_online is None:
+                is_online = False
             yield {"userId": user_id, "status": is_online}
             await asyncio.sleep(11)  # Check every 10 seconds
 
 @subscription.field("onlineStatus")
 async def resolve_online_status(status, info, user_id):
     return status
-
 
 resolver = [query, mutation, subscription]
