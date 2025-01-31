e44fd954fce172c9325b9ea5c773f549370fb664
nscheefe <nils.scheefe@gmail.com>
2025-01-31 02:04:36 +0100

    [Frontend] Refactor scrollable components and update styles.

--- app/django_service/frontend_service/frontend_service/static/js/community.js
+++ app/django_service/frontend_service/frontend_service/static/js/community.js
@@ -205,7 +205,7 @@ const renderUserProfile = async (user, stats, statsByUser, profileContainer) => 
                 </div>
 
                 <!-- Detailed User Stats -->
-                <div class="stats-list mt-3">
+                <div class="stats-list mt-3 scrollable">
                     ${statsByUser.map((stat, index) => {
                         const date = stat.stat.createdAt;
                         const result = stat.didWin ? 'win' : 'loss';
