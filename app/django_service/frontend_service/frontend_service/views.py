c4e77e557dfc8c7c4445837b1c7e9291b7fc9a5a
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2025-01-31 01:25:09 +0000

    [frontend] Update: Add game ID to context in game view

--- app/django_service/frontend_service/frontend_service/views.py
+++ app/django_service/frontend_service/frontend_service/views.py
@@ -70,16 +70,18 @@ def home2(request):
 @jwt_required
 def game(request):
     context = get_home_context(request)
-    game = request.GET.get('game', None)
+    game = None
+    gameId = request.GET.get('game', None)
     # Check for the 'local' query parameter
     is_local_game = request.GET.get('local', 'false').lower() == 'true'
-    if is_local_game != True:
+    if is_local_game != True and gameId != None:
         game = create_game.createGame(request)
         logger.info(game)
 
     # Extend the context with additional data for the game view
     context.update({
         'game': game,  # Example function for getting game data
+        'game_id': gameId,  # Add the game ID to the context
         'is_local_game': is_local_game,  # Add the local game flag to the context
     })
 
